a84dc03e104c6d876ab44af367bab84c4c7a95d7
Martin Aspeli <optilude@gmail.com>
2021-06-02 22:00:16 +0000

    Sketch + docs for parsing of matches and blocks

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ The other mandatory key is "Sheet", which can take the operator "is" or "matches
 
 Next must come some search criteria.
 
-A single cell can be matched by either reference or value. To match by reference, use the key "Reference" with operator "is" and a value that specifies either a cell reference (e.g. "A3") or an Excel defined name at sheet or workbook level.
+A single cell can be matched by either reference or value. To match by reference, use the key "Cell" with operator "is" and a value that specifies either a cell reference (e.g. "A3") or an Excel defined name at sheet or workbook level.
 
 To search for a cell value, use the key "Value" with operators "is" (equality), "<", "<=", ">", ">=", "is empty", "is not empty", or "matches" (a regular expression). It is possible to match a cell that is offset from the one found by value (e.g. one column after, or two rows above) by using the keys "Row offset" or "Column offset". These use the operator "is" and take a number which can be positive (after/below) or negative (before/above), e.g. "Row offset" | "is" | -2.
 
@@ -27,21 +27,21 @@ Finally, the search area can be constrained by using the keys "Min row", "Min co
 
 Matching a range or table allows you to populate a table or search for a cell by row and column label.
 
-The easiest way to match a table is by giving a "Reference" that "is" a range (e.g. "A3:C4") or a defined name specifying a range, or a named Excel table.
+The easiest way to match a table is by giving a "Table" that "is" a range (e.g. "A3:C4") or a defined name specifying a range, or a named Excel table.
 
 If that’s not possible (i.e. the structure of the source workbook is not sufficiently stable), you can search for it by finding the start cell (top/left) and giving one of an end cell (bottom/right); a fixed number of rows and columns; or assuming that the table extends across the contiguous width of the header row and height of the first column (i.e. until a blank cell is found).
 
-Searching for the start cell uses a cell match as described above, but each key is prefixed by the word "Start", so it can be identified by "Start reference" or "Start value", optionally with "Start row offset" or "Start column offset". Do not specify a sheet or search area for the start cell specifically (i.e. there are no "Start sheet" or "Start min row" parameters).
+Searching for the start cell uses a cell match as described above, but each key is prefixed by the word "Start", so it can be identified by "Start cell" or "Start value", optionally with "Start row offset" or "Start column offset". Do not specify a sheet or search area for the start cell specifically (i.e. there are no "Start sheet" or "Start min row" parameters).
 
 If you specify only a start cell, the table dimensions will be found by looking for a contiguous row of header cells and a contiguous first column (empty cells are allowed within the table).
 
-To specify an end cell, use the same approach with the prefix "End", e.g. "End reference" or "End value".
+To specify an end cell, use the same approach with the prefix "End", e.g. "End cell" or "End value".
 
 To specify a fixed size, use the keys "Rows" and "Columns" with the operator "is" and a number for each.
 
 As with single cells, the search area can be constrained using "Min row", "Max row", "Min column" and "Max column". These are specified at thet top level, i.e. not for the start or end cell if used.
 
-Once we have matched the cell, the next step is to identify the target. The target is actually optional, because it can be useful to match a cell and then use its value as a variable in another match, but most blocks will specify a target, using a row like "Target" | "is" | "<reference>". Targets can only be specified by a cell/range reference ("A3" or "A3:C7"), a defined name at workbook level (i.e. not a sheet-specific name), or as a named Excel table. The assumption is that you have enough control over the target workbook to not need to search by value or offset.
+Once we have matched the cell, the next step is to identify the target. The target is actually optional, because it can be useful to match a cell and then use its value as a variable in another match, but most blocks will specify a target, using a row like "Target cell" | "is" | "<reference>". Targets can only be specified by a cell/range reference ("A3" or "A3:C7"), a defined name at workbook level (i.e. not a sheet-specific name), or as a named Excel table. The assumption is that you have enough control over the target workbook to not need to search by value or offset. Use "Target cell" for cells and "Target table" for ranges.
 
 The simplest scenario is where you match a single cell and copy it to a single target. Simplify specify a single-cell target range or name, and the the matched value will be copied over.
 
--- src/xlsx_extract/config.py
+++ src/xlsx_extract/config.py
@@ -6,6 +6,7 @@ from typing import Any, Dict, List, Match, Tuple, Union
 import openpyxl
 
 from openpyxl.workbook.workbook import Workbook
+from openpyxl.utils.cell import column_index_from_string
 from openpyxl.utils.exceptions import InvalidFileException
 
 from .range import Range
@@ -32,40 +33,47 @@ class MatchKeys:
     # Sheet to search
     SHEET = "sheet"
 
-    # A cell reference, defined name, or named table
-    REFERENCE = "reference"
-
-    # Target reference
-    TARGET = "target"
-
-    # Whether to resize target table to match source (or truncate)
-    EXPAND = "expand"
-
-    # Whether to align source and target table
-    ALIGN = "align"
-
-class CellMatchKeys:
+class CellMatchKeys(MatchKeys):
     """Keys specific to cell matches
     """
+
+    # A reference to a single cell (coordinate or name)
+    CELL_REFERENCE = "cell"
 
     VALUE = "value"
 
     MIN_ROW = "min row"
     MAX_ROW = "max row"
     MIN_COL = "min column"
-    MAX_CAL = "max column"
+    MAX_COL = "max column"
 
     ROW_OFFSET = "row offset"
     COL_OFFSET = "column offset"
 
-class RangeMatchKeys:
+class RangeMatchKeys(MatchKeys):
     """Keys specific to range matches
     """
 
+    # A reference to a range, defined name pointing to a table, or named table
+    TABLE_REFERENCE = "table"
+
+    # Size of matched table
     ROWS = "rows"
     COLS = "columns"
 
-class CellMatchPrefixes:
+class TargetKeys:
+
+    # Target reference
+    TARGET_CELL = "target cell"
+    TARGET_TABLE = "target table"
+
+    # Whether to align source and target table
+    ALIGN = "align"
+
+    # Whether to resize target table to match source (or truncate)
+    EXPAND = "expand"
+
+class Prefix:
     """Prefixes used for finding cells within a table
     """
 
@@ -78,6 +86,34 @@ class CellMatchPrefixes:
     TARGET_ROW = "target row"
     TARGET_COL = "target column"
 
+    @classmethod
+    def none(cls, s):
+        return s
+
+    @classmethod
+    def start(cls, s):
+        return "%s %s" % (cls.START, s)
+    
+    @classmethod
+    def end(cls, s):
+        return "%s %s" % (cls.END, s)
+    
+    @classmethod
+    def source_row(cls, s):
+        return "%s %s" % (cls.SOURCE_ROW, s)
+    
+    @classmethod
+    def source_col(cls, s):
+        return "%s %s" % (cls.SOURCE_COL, s)
+
+    @classmethod
+    def target_row(cls, s):
+        return "%s %s" % (cls.TARGET_ROW, s)
+    
+    @classmethod
+    def target_col(cls, s):
+        return "%s %s" % (cls.TARGET_COL, s)
+    
 Operators = {
     "is": Operator.EQUAL,
     "=": Operator.EQUAL,
@@ -225,7 +261,7 @@ def run(target_workbook : Workbook, source_directory : str, config_sheet : str =
                 continue
 
             try:
-                source_match = extract_source_match(block, source_workbook)
+                source_match = extract_source_match(block)
             except AssertionError as e:
                 # An assertion failed during match construction
                 history.append(Action(block_name, False, str(e)))
@@ -341,7 +377,6 @@ def extract_filename(block : Dict[str, Comparator], current_directory : str) -> 
     
     assert os.path.isdir(current_directory), "Directory `%s` not found" % current_directory
 
-
     with_dir = lambda f: os.path.join(current_directory, f)
     is_file = lambda f: os.path.isfile(with_dir(f))
 
@@ -367,22 +402,132 @@ def extract_filename(block : Dict[str, Comparator], current_directory : str) -> 
     
     return (with_dir(filename), match,)
 
+def _cast_col(col):
+    if isinstance(col, (str, bytes,)):
+        try:
+            col = column_index_from_string(col)
+        except ValueError:
+            pass
+    assert col is None or isinstance(col, int), "%s is not a valid column name" % col
+    return col
 
-def extract_source_match(block : Dict[str, Comparator], source_workbook : Workbook) -> Match:
-    """
+def _contains_cell_match(block, prefix=Prefix.none):
+    return prefix(CellMatchKeys.CELL_REFERENCE) in block or prefix(CellMatchKeys.VALUE)
+
+def _build_cell_match(block : Dict[str, Comparator], name, sheet=None, prefix=Prefix.none) -> CellMatch:
+
+    sheet = block[prefix(CellMatchKeys.SHEET)] if prefix(CellMatchKeys.SHEET) in block else sheet
+    reference = block[prefix(CellMatchKeys.CELL_REFERENCE)].value if prefix(CellMatchKeys.CELL_REFERENCE) in block else None
+    value = block[prefix(CellMatchKeys.VALUE)] if prefix(CellMatchKeys.VALUE) in block else None
+    row_offset = block[prefix(CellMatchKeys.ROW_OFFSET)].value if prefix(CellMatchKeys.ROW_OFFSET) in block else None
+    col_offset = block[prefix(CellMatchKeys.COL_OFFSET)].value if prefix(CellMatchKeys.COL_OFFSET) in block else None
+    min_row = block[prefix(CellMatchKeys.MIN_ROW)].value if prefix(CellMatchKeys.MIN_ROW) in block else None
+    max_row = block[prefix(CellMatchKeys.MAX_ROW)].value if prefix(CellMatchKeys.MAX_ROW) in block else None
+    min_col = _cast_col(block[prefix(CellMatchKeys.MIN_COL)].value) if prefix(CellMatchKeys.MIN_COL) in block else None
+    max_col = _cast_col(block[prefix(CellMatchKeys.MAX_COL)].value) if prefix(CellMatchKeys.MAX_COL) in block else None
+    
+    assert name is not None and isinstance(name, (str, bytes,)), "Block name must be a string"
+    assert reference is None or isinstance(reference, (str, bytes,)), "Cell reference must be a string"
+    assert row_offset is None or isinstance(row_offset, int), "Row offset must be a number"
+    assert col_offset is None or isinstance(col_offset, int), "Column offset must be a number"
+    assert min_row is None or isinstance(min_row, int), "Min row must be a number"
+    assert max_row is None or isinstance(max_row, int), "Max row must be a number"
+    assert min_col is None or isinstance(min_col, int), "Min column must be a number"
+    assert max_col is None or isinstance(max_col, int), "Max column must be a number"
+    
+    return CellMatch(
+        name=name,
+        sheet=sheet,
+        reference=reference,
+        value=value,
+        row_offset=row_offset,
+        col_offset=col_offset,
+        min_row=min_row,
+        max_row=max_row,
+        min_col=min_col,
+        max_col=max_col,
+    )
+
+def extract_source_match(block : Dict[str, Comparator]) -> Match:
+    """Create a CellMatch or RangeMatch from the block if possible
     """
 
-    # TODO: Construct CellMatch or RangeMatch
+    is_cell_match = MatchKeys.NAME in block and _contains_cell_match(block)
+
+    is_range_match = MatchKeys.NAME in block and (
+        RangeMatchKeys.TABLE_REFERENCE in block or
+        _contains_cell_match(block, Prefix.start)
+    )
+
+    if not is_cell_match and not is_range_match:
+        return None
+    
+    assert not (is_cell_match and is_cell_match), "Block refers to both a cell and a range"
+
+    name = block[MatchKeys.NAME].value
+
+    if is_cell_match:
+        return _build_cell_match(block, name)
+    elif is_range_match:
+        sheet = block[RangeMatchKeys.SHEET] if RangeMatchKeys.SHEET in block else None
+        reference = block[RangeMatchKeys.TABLE_REFERENCE].value if RangeMatchKeys.TABLE_REFERENCE in block else None
+        rows = block[RangeMatchKeys.ROWS].value if RangeMatchKeys.ROWS in block else None
+        cols = block[RangeMatchKeys.COLS].value if RangeMatchKeys.COLS in block else None
+        start_cell = _build_cell_match(block, "%s:start" % name, sheet, Prefix.start) if _contains_cell_match(block, Prefix.start) else None
+        end_cell = _build_cell_match(block, "%s:end" % name, sheet, Prefix.end) if _contains_cell_match(block, Prefix.end) else None
+
+        assert reference is None or isinstance(reference, (str, bytes,)), "Table reference must be a string"
+        assert rows is None or isinstance(rows, int), "Rows must be a number"
+        assert cols is None or isinstance(cols, int), "Rows must be a number"
+
+        return RangeMatch(
+            name=name,
+            sheet=sheet,
+            reference=reference,
+            start_cell=start_cell,
+            end_cell=end_cell,
+            rows=rows,
+            cols=cols,
+        )
 
     return None
 
 def extract_target(block : Dict[str, Comparator], source_match : Match) -> Target:
-    """
+    """Create a Target from the block if possible
     """
 
-    # TODO: Construct Target
+    if not (TargetKeys.TARGET_CELL in block or TargetKeys.TARGET_TABLE in block):
+        return None
+    
+    target_cell = block[TargetKeys.TARGET_CELL].value if TargetKeys.TARGET_CELL in block else None
+    target_table = block[TargetKeys.TARGET_TABLE].value if TargetKeys.TARGET_TABLE in block else None
 
-    return None
+    align = block[TargetKeys.ALIGN].value if TargetKeys.ALIGN in block else None
+    expand = block[TargetKeys.EXPAND].value if TargetKeys.ALIGN in block else None
+
+    source_row = _build_cell_match(block, "%s:source_row" % source_match.name, source_match.sheet, Prefix.source_row) if _contains_cell_match(block, Prefix.source_row) else None
+    source_col = _build_cell_match(block, "%s:source_col" % source_match.name, source_match.sheet, Prefix.source_col) if _contains_cell_match(block, Prefix.source_col) else None
+
+    target_row = _build_cell_match(block, "%s:target_row" % source_match.name, None, Prefix.target_row) if _contains_cell_match(block, Prefix.target_row) else None
+    target_col = _build_cell_match(block, "%s:target_col" % source_match.name, None, Prefix.target_col) if _contains_cell_match(block, Prefix.target_col) else None
+
+    assert not (target_cell is not None and target_table is not None), "Only one of 'Target cell' and 'Target table' should be given"    
+    assert target_cell is None or isinstance(target_cell, (str, bytes,)), "Target cell reference must be a string"
+    assert target_table is None or isinstance(target_table, (str, bytes,)), "Target table reference must be a string"
+    assert align is None or isinstance(align, bool), "Align must be a boolean (true/false)"
+    assert expand is None or isinstance(align, bool), "Expand must be a boolean (true/false)"
+
+    target_match = CellMatch("%s:target" % source_match.name, reference=target_cell) if target_cell is not None \
+            else RangeMatch("%s:target" % source_match.name, reference=target_table)
+
+    return Target(
+        source=source_match,
+        target=target_match,
+        source_row=source_row,
+        source_col=source_col,
+        target_row=target_row,
+        target_col=target_col,
+    )
 
 def interpolate_variables(value : str, variables : Dict[str, Any]) -> str:
     """Interpolate variables into the value string
